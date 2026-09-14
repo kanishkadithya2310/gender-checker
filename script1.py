1533456cdd4531764da103826ce91351de8a7dab
kanishkadithya2310 <kanishkadithya2000@gmail.com>
2026-09-14 14:29:14 +0530

    Add files via upload

--- script1.py
+++ script1.py
@@ -1,10 +1,12 @@
 
-gender = input("Are you male? yes or No : ")
-if gender =="yes":
-    print("you are male")
-if gender =="No":
-    print("you are female")
+import streamlit as st
 
+gender = st.text_input("Are you male? yes or No :")
+
+if gender == "yes":
+    st.write("You are male")
+elif gender == "No":
+    st.write("You are female")
 
 
 
